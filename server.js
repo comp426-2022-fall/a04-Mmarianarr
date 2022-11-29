36bed26b12fbc66a8669524af72821dc2a5b1566
Mariana Rodriguez <marianar@email.unc.edu>
2022-11-28 20:23:20 -0500

    Added log file.

---
+++ server.js
@@ -0,0 +1,61 @@
+import {roll} from "./lib/roll.js";	
+import express from "express";
+import minimist from "minimist";
+	
+const app = express();
+	
+const args = minimist(process.argv.slice(2));
+	
+args['port'];
+const port = args.port || 5000;
+	
+const server = app.listen(port);
+	
+	
+app.get('/app/', (req, res) => {
+res.send('200 OK')
+res.end;
+});
+	
+app.get('/app/roll/', (req, res) => {
+const sides =6;
+const dice = 2;
+const rolls =1;
+res.send(roll(sides, dice, rolls));
+res.end;
+});
+	
+	
+app.post('/app/roll/', (req, res) => {
+const sides = 6 || req.body.sides;
+const dice = 2 || req.body.dice ;
+const rolls =  1 || req.body.rolls;
+res.send(roll(sides, dice, rolls));
+res.end;
+});
+	
+app.get('/app/roll/:sides/', (req, res)=> {
+const sides = req.params.sides;
+res.send(roll(sides, 2, 1))
+res.end
+});
+	
+app.get('/app/roll/:sides/:dice/', (req, res)=> {
+const sides = req.params.sides;
+const dice = req.params.dice;
+res.send(roll(sides, dice, 1));
+res.end
+});
+	
+app.get('/app/roll/:sides/:dice/:rolls', (req, res)=> {
+const sides = req.params.sides;
+const dice = req.params.dice;
+const rolls = req.params.rolls
+res.send(roll(sides, dice, rolls));
+res.end
+});
+	
+app.use(function(req, res){
+res.status(404).send("404 NOT FOUND")
+});
+
