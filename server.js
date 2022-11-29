5298022e1bfd931ecd6e11d8594ab274a58f4f5b
Mariana Rodriguez <marianar@email.unc.edu>
2022-11-29 12:49:08 -0500

    made more changes.

--- server.js
+++ server.js
@@ -1,51 +1,54 @@
+#!/usr/bin/env node
 	
-	import { roll } from './lib/roll.js';
-	import minimist from 'minimist';
-	import express from 'express';
+import {roll} from './lib/roll.js';
+	
+import express from 'express';
+const app = express();
+
+import minimist from 'minimist';
+
+const args = minimist(process.argv.slice(2));
+
+const port = args.port || 5000;
+	
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 	
 
-	const app = express()
-	const args = minimist(process.argv.slice(2));
-	const port = args.port || 5000
+app.get('/app/', function (req, res) {
+res.send('200 OK');
+});
 	
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
+app.get('/app/roll/', function (req, res) {
+res.send(roll(6, 2, 1));
+});
 	
-	// parsing
-	app.get('/app/', function (req, res) {
-	res.send('200 OK');
-	})
+app.get('/app/roll/*/', function (req, res) {
+let url = req.url;
+let array = url.split("/");
+res.send(roll(array[3], 2, 1));
+});
 	
-	app.get('/app/roll/', function (req, res) {
-	res.send(roll(6, 2, 1));
-	})
+app.get('/app/roll/*/*/', function (req, res) {
+let url = req.url;
+let array = url.split("/");
+res.send(roll(array[3], array[4], 1));
+});
 	
-	app.get('/app/roll/*/', function (req, res) {
-	let url = req.url;
-	let array = url.split("/");
-	res.send(roll(array[3], 2, 1))
-	})
-	
-	app.get('/app/roll/*/*/', function (req, res) {
-	let url = req.url;
-	let array = url.split("/");
-	res.send(roll(array[3], array[4], 1))
-	})
-	
-	app.get('/app/roll/*/*/*/', function (req, res) {
-	let url = req.url;
-	let array = url.split("/");
-	res.send(roll(array[3], array[4], array[5]))
-	})
+app.get('/app/roll/*/*/*/', function (req, res) {
+let url = req.url;
+let array = url.split("/");
+res.send(roll(array[3], array[4], array[5]));
+});
 	
 	
-	app.get('*', function (req, res) {
-	res.send('404 NOT FOUND');
-	})
+app.get('*', function (req, res) {
+res.send('404 NOT FOUND');
+});
 	
-	app.post('/app/roll/', function(req, res) {
-	res.send(roll(req.body.sides, req.body.dice, req.body.rolls));
-	});
+app.post('/app/roll/', function(req, res) {
+res.send(roll(req.body.sides, req.body.dice, req.body.rolls));
+});
 	
-	app.listen(port)
+app.listen(port);
 
